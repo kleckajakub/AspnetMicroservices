812b4eb2b40e268776f9424d3115e3caeefd7299
Jakub Klečka <jakub.klecka@geovap.cz>
2023-10-03 07:03:39 +0200

    Discount.API - run in docker

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Discount.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Discount.API {
   public class Program {
     public static void Main(string[] args) {
-      CreateHostBuilder(args).Build().Run();
+      CreateHostBuilder(args).Build()
+                             .MigrateDatabase<Program>()
+                             .Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
