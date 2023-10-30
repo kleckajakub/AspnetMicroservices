7dd2ece2f96ca803caa7e97dd368e18d7d138dfe
Jakub Klečka <jakub.klecka@geovap.cz>
2023-10-30 07:49:27 +0100

    Ordering - contracts and models

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ordering.Domain.Common {
+  public abstract class EntityBase {
+    public int Id { get; protected set; }
+    public DateTime CreatedDate { get; protected set; }
+    public string CreatedBy { get; protected set; }
+    public DateTime? LastModifiedDate { get; protected set; }
+    public string LastModifiedBy { get; protected set; }
+  }
+}
